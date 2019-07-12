487e35ebd11bd47287980546d8eebd538e17cb47
Angela Jane Leung <angelajaneleung@gmail.com>
2019-07-11 20:00:27 -0400

    some functions in js all commented out

--- assets/app.js
+++ assets/app.js
@@ -46,6 +46,76 @@ $("#quoteButton").on("click", function () {
   quote();
 });
 
+// //Functions for Play,Pause, Stop
+// var intervalID;
+
+// //function for 1 minute selection
+// var number= 1;
+
+//    function minuteBtn1(){
+//    clearInterval(intervalID);
+//    intervalID = setInterval(decrement, 60000);}
+
+//    function decrement(){
+//     number--;
+//     console.log(number)
+//     if ( number === 0){
+//       stop();
+//      }
+//     }
+// //function for 5 minute selection 
+// var timer5= 5;
+// //function for 10 minute selection
+// var timer10= 10;
+
+// //Play Button
+// $(".BeginBtn").on("click", function(){
+
+//   if ("#minuteBtn1"){
+//     minuteBtn1()
+//   }
+//   console.log("playbutton");
+// })
+// //Pause Button
+
+// function RecurringTimer(callback, delay) {
+//   var timerId, start, remaining = delay;
+
+//   this.pause = function() {
+//       window.clearTimeout(timerId);
+//       remaining -= new Date() - start;
+//   };
+
+//   var resume = function() {
+//       start = new Date();
+//       timerId = window.setTimeout(function() {
+//           remaining = delay;
+//           resume();
+//           callback();
+//       }, remaining);
+//   };
+
+//   this.resume = resume;
+
+//   this.resume();
+// }
+// //Stop Function
+// function stop (){
+//   clearInterval(intervalID)
+// }
+// //Timeut Function
+// function timeout(){
+
+//   clearInterval(intervalID)
+
+// }
+// //Stop Button/Timeout
+
+
+
+
+
+
 // Create function for weather status
 $("#add-city").on("click", function (event) {
 
