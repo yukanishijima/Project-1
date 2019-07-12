e9588945723071c385c622eda4a9c1e07b62d146
Yuka Nishijima <48897211+yukanishijima@users.noreply.github.com>
2019-07-11 21:40:42 -0400

    Add play button

--- assets/app.js
+++ assets/app.js
@@ -30,7 +30,7 @@ $("#startBtn").on("click", function () {
     $("#customizeTime").hide();
     $("#modalMsg").html("<p>Welcome " + userName + ", select a time!</p>");
 
-    db.ref().set({
+    db.ref().push({
       name: userName
     });
   }
@@ -39,11 +39,10 @@ $("#startBtn").on("click", function () {
 // timer modal function
 $("#minuteBtn1").on("click", function (event) {
   event.preventDefault();
-
   $("#timerModal").modal("hide");
   $("#welcomeModal").modal("hide");
 
-  userTime = $("#minuteBtn1").attr("val") * 10000;
+  userTime = $("#minuteBtn1").attr("val") * 60;
   db.ref().update({
     time: userTime,
     dateAdded: firebase.database.ServerValue.TIMESTAMP
@@ -52,11 +51,10 @@ $("#minuteBtn1").on("click", function (event) {
 
 $("#minuteBtn2").on("click", function (event) {
   event.preventDefault();
-
   $("#timerModal").modal("hide");
   $("#welcomeModal").modal("hide");
 
-  userTime = $("#minuteBtn2").attr("val") * 10000;
+  userTime = $("#minuteBtn2").attr("val") * 60;
   db.ref().update({
     time: userTime,
     dateAdded: firebase.database.ServerValue.TIMESTAMP
@@ -65,11 +63,10 @@ $("#minuteBtn2").on("click", function (event) {
 
 $("#minuteBtn3").on("click", function (event) {
   event.preventDefault();
-
   $("#timerModal").modal("hide");
   $("#welcomeModal").modal("hide");
 
-  userTime = $("#minuteBtn3").attr("val") * 10000;
+  userTime = $("#minuteBtn3").attr("val") * 60;
   db.ref().update({
     time: userTime,
     dateAdded: firebase.database.ServerValue.TIMESTAMP
@@ -82,7 +79,6 @@ $("#customizeBtn").on("click", function () {
 
 $("#submitBtn").on("click", function (event) {
   event.preventDefault();
-
   $("#timerModal").modal("hide");
   $("#welcomeModal").modal("hide");
 
@@ -99,33 +95,86 @@ $("#submitBtn").on("click", function (event) {
   });
 });
 
+//Functions for Play,Pause, Stop
+var intervalID;
+var number = userTime;
 
+function startTimer() {
+  number = userTime;
+  console.log(userTime);
+  clearInterval(intervalID);
+  intervalID = setInterval(decrement, 1000);
+}
+function decrement() {
+  number--;
+  if (number === 0) {
+    stop();
+  }
+}
 
+//Play Button
+$(".BeginBtn").on("click", function () {
+  startTimer();
+})
+
+//Stop Function
+function stop() {
+  clearInterval(intervalID)
+}
+
+// //Timeut Function
+// function timeout() {
+//   clearInterval(intervalID)
+// }
+
+// //Pause Button
+
+// function RecurringTimer(callback, delay) {
+//   var timerId, start, remaining = delay;
+
+//   this.pause = function () {
+//     window.clearTimeout(timerId);
+//     remaining -= new Date() - start;
+//   };
+
+//   var resume = function () {
+//     start = new Date();
+//     timerId = window.setTimeout(function () {
+//       remaining = delay;
+//       resume();
+//       callback();
+//     }, remaining);
+//   };
+
+//   this.resume = resume;
+
+//   this.resume();
+// }
 
 
 // Create quote function
 function quote() {
-    $.ajax ({
-      url: "https://api.forismatic.com/api/1.0/",
-      jsonp: "jsonp",
-      dataType: "jsonp",
-      data: {
-        method: "getQuote",
-        lang: "en",
-        format: "jsonp"
-      },
-      success: function(response) {
-        $('#quote').html(response.quoteText)
-        $('#author').html("<br/>&dash; " + response.quoteAuthor)
-  
-      }
-    });
-  }
-  
-  $("#quoteButton").on("click", function() {
-  
-    quote();
+  $.ajax({
+    url: "https://api.forismatic.com/api/1.0/",
+    jsonp: "jsonp",
+    dataType: "jsonp",
+    data: {
+      method: "getQuote",
+      lang: "en",
+      format: "jsonp"
+    },
+    success: function (response) {
+      $('#quote').html(response.quoteText)
+      $('#author').html("<br/>&dash; " + response.quoteAuthor)
+
+    }
   });
+}
+
+$("#quoteButton").on("click", function () {
+
+  quote();
+});
 
 // $("#quoteButton").on("click", function () {
 //     console.log("quote!")
@@ -137,34 +186,35 @@ function quote() {
 
 $("#add-city").on("click", function (event) {
 
-    event.preventDefault();
+  event.preventDefault();
 
-    let location = $("#location-input").val().trim();
+  let location = $("#location-input").val().trim();
 
-    var APIKey = "d15b9c0cdc5ef1799aacbfc05974ff66";
+  var APIKey = "d15b9c0cdc5ef1799aacbfc05974ff66";
 
-    // Here we are building the URL we need to query the database
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
-        location + "&units=metric&appid=" + APIKey;
+  // Here we are building the URL we need to query the database
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
+    location + "&units=metric&appid=" + APIKey;
 
-    // Here we run our AJAX call to the OpenWeatherMap API
-    $.ajax({
-        url: queryURL,
-        method: "GET"
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function (response) {
+
+      // Log the queryURL
+      console.log(queryURL);
+
+      // Log the resulting object
+      console.log(response);
+
+      // Transfer content to HTML
+      $(".city").html("<h1>" + response.name + " Weather </h1>");
+      $(".current-temp").html("<h4>Current Temperature (C): " + response.main.temp + "&#8451;</h4>");
+      $(".max-temp").html("<h4>Today's High (C): " + response.main.temp_max + "&#8451;</h4>");
+      $(".min-temp").html("<h4>Today's Low (C): " + response.main.temp_min + "&#8451;</h4>");
+
     })
-        // We store all of the retrieved data inside of an object called "response"
-        .then(function (response) {
-
-            // Log the queryURL
-            console.log(queryURL);
-
-            // Log the resulting object
-            console.log(response);
-
-            // Transfer content to HTML
-            $(".city").html("<h1>" + response.name + " Weather </h1>");
-            $(".current-temp").html("<h4>Current Temperature (C): " + response.main.temp + "&#8451;</h4>");
-            $(".max-temp").html("<h4>Today's High (C): " + response.main.temp_max + "&#8451;</h4>");
-            $(".min-temp").html("<h4>Today's Low (C): " + response.main.temp_min + "&#8451;</h4>");
-
-        })});
+});
